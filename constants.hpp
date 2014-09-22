d65f0d8ed3011bc0d0bb5835278a02632447d094
JuneHyeon Bae <devunt@gmail.com>
2014-09-22 12:45:11 +0900

    Use gd2k server instead of gd2

--- constants.hpp
+++ constants.hpp
@@ -2,10 +2,10 @@
 
 #define LINE_PRPL_ID "prpl-mvirkkunen-line"
 
-#define LINE_THRIFT_SERVER "gd2.line.naver.jp"
+#define LINE_THRIFT_SERVER "gd2k.line.naver.jp"
 #define LINE_OS_URL "https://os.line.naver.jp/"
 #define LINE_STICKER_URL "http://dl.stickershop.line.naver.jp/products/"
-#define LINE_VERIFICATION_URL "https://gd2.line.naver.jp/Q"
+#define LINE_VERIFICATION_URL "https://gd2k.line.naver.jp/Q"
 #define LINE_SESSION_URL "https://gd2k.line.naver.jp/authct/v1/keys/naver"
 
 #define LINE_LOGIN_PATH "/api/v4/TalkService.do"
